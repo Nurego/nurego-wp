a9fe69e7f66794a424b86c200f633166ea3150f7
Bronky <erik.barzdukas@gmail.com>
2014-07-23 09:57:09 -0700

    Added register call for settings

--- includes/settings.php
+++ includes/settings.php
@@ -13,8 +13,34 @@ function register_nwp_submenu_page() {
 function nwp_custom_submenu_page_callback() {
 
     echo '<div class="wrap"><div id="icon-tools" class="icon32"></div>';
-        echo '<h2>Nurego WordPress Setting Page</h2>';
+    echo '<h2>Nurego WordPress Setting Page</h2>';
+    echo '<p>';
+    echo '<form method="POST" action="options.php">';
+
     echo '</div>';
+}
+
+/**
+ * Register all of the settings we intend to show on the settings page
+ */
+function register_nwp_settings() {
+    register_setting('nwp_settings_group', 'element_id');
+    register_setting('nwp_settings_group', 'theme');
+    register_setting('nwp_settings_group', 'css_url');
+    register_setting('nwp_settings_group', 'select_url');
+    register_setting('nwp_settings_group', 'select_callback');
+    register_setting('nwp_settings_group', 'label_price');
+    register_setting('nwp_settings_group', 'label_select');
+    register_setting('nwp_settings_group', 'label_feature_on');
+    register_setting('nwp_settings_group', 'label_feature_on');
+    register_setting('nwp_settings_group', 'label_before_price');
+    register_setting('nwp_settings_group', 'label_after_price');
+    register_setting('nwp_settings_group', 'time_out');
+    register_setting('nwp_settings_group', 'loading_class');
+    register_setting('nwp_settings_group', 'error_class');
+    register_setting('nwp_settings_group', 'warning_class');
+    register_setting('nwp_settings_group', 'empty_class');
+    register_setting('nwp_settings_group', 'price_class');
 }
 
 // Hook in the submenu
